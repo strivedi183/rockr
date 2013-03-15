cdb4936d4f9b5804f920a4fb663c568ec3526e89
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-15 09:04:30 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rockr::Application.config.secret_token = 'c54f81d18678b58531029669b7601efa50fbea8b6588ec0ef344abf55ebfb012bf2a2df0e35744848101f528e1647529e12a3b834fdc8b1db4a6656dbf8e2a69'
